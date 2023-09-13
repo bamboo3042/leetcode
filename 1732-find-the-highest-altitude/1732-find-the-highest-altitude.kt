2d33be3907804a41e702c256803ee6374698e64e
bamboo3042 <39301206+bamboo3042@users.noreply.github.com>
2023-09-13 11:26:03 +0900

    Time: 172 ms (10.05%), Space: 37.8 MB (6.03%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.kt
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.kt
@@ -1,11 +1,5 @@
 class Solution {
     fun largestAltitude(gain: IntArray): Int {
-        val result = IntArray(gain.size + 1) { 0 }
-        
-        for(i in gain.indices) {
-            result[i+1] = result[i] + gain[i]
-        }
-        
-        return result.max()
+        return gain.fold(intArrayOf(0)) { acc, i -> acc + (acc.last() + i)}.max()
     }
 }
