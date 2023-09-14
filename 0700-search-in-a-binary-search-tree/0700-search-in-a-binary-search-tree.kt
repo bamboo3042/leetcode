a623d29cd9b229c57ecee9e337b56c88ac40824f
bamboo3042 <39301206+bamboo3042@users.noreply.github.com>
2023-09-14 18:18:24 +0900

    Time: 275 ms (9.45%), Space: 38.6 MB (22.39%) - LeetHub

---
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.kt
@@ -0,0 +1,18 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun searchBST(root: TreeNode?, `val`: Int): TreeNode? {
+        return if(root == null) null
+        else if(root.`val` == `val`) root
+        else if(root.`val` > `val`) searchBST(root.left, `val`)
+        else searchBST(root.right, `val`)
+    }
+}
