66cdfeee5fd1d5879aa3244fe6ba7c2b78bda679
bamboo3042 <39301206+bamboo3042@users.noreply.github.com>
2023-09-21 12:17:43 +0900

    Time: 198 ms (32.45%), Space: 38.1 MB (21.85%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.kt
+++ 0994-rotting-oranges/0994-rotting-oranges.kt
@@ -4,32 +4,33 @@ class Solution {
         val dx = arrayOf(1, -1, 0, 0)
         val dy = arrayOf(0, 0, 1, -1)
         val queue = ArrayDeque<Triple<Int, Int, Int>>()
-        val list = hashSetOf<Pair<Int, Int>>()
-        
-        grid.forEachIndexed { i, ints -> 
-            ints.forEachIndexed { j, o -> 
+        var count = 0
+
+        grid.forEachIndexed { i, ints ->
+            ints.forEachIndexed { j, o ->
                 if(o == 2) queue.add(Triple(i, j, 0))
-                else if(o == 1) list.add(i to j)
+                else if(o == 1) count++
             }
         }
-        
+
         while(queue.isNotEmpty()) {
             val (x, y, d) = queue.removeFirst()
-            
+
             result = maxOf(result, d)
-            
+
             repeat(4) {
                 val nx = x + dx[it]
                 val ny = y + dy[it]
-                
-                if(nx in grid.indices && ny in grid[0].indices && list.contains(nx to ny)) {
-                    list.remove(nx to ny)
+
+                if(nx in grid.indices && ny in grid[0].indices && grid[nx][ny] == 1) {
+                    grid[nx][ny] = 2
+                    count--
                     queue.addLast(Triple(nx, ny, d+1))
                 }
             }
         }
-        
-        return if(list.isNotEmpty()) -1 
+
+        return if(count != 0) -1
         else result
     }
 }
