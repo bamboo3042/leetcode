ac586905cef345a9a5d8787f7f11e0626fd387c4
bamboo3042 <39301206+bamboo3042@users.noreply.github.com>
2023-09-21 17:29:33 +0900

    Time: 767 ms (53.13%), Space: 56.5 MB (81.25%) - LeetHub

--- 2542-maximum-subsequence-score/2542-maximum-subsequence-score.kt
+++ 2542-maximum-subsequence-score/2542-maximum-subsequence-score.kt
@@ -1,24 +1,22 @@
 class Solution {
     fun maxScore(nums1: IntArray, nums2: IntArray, k: Int): Long {
         val n = nums1.size
-        val nums = Array(n) { Num(nums1[it], nums2[it]) }
+        val nums = Array(n) { nums1[it] to nums2[it] }
 
-        nums.sortWith(compareByDescending { it.num2 })
+        nums.sortWith(compareByDescending { it.second })
 
         val pq = PriorityQueue<Int>()
         var res = 0L
         var sum = 0L
 
         for (i in 0 until n) {
-            pq.offer(nums[i].num1)
-            sum += nums[i].num1.toLong()
+            pq.offer(nums[i].first)
+            sum += nums[i].first.toLong()
 
             if (pq.size > k) sum -= pq.poll()
-            if (pq.size == k) res = maxOf(res, sum * nums[i].num2.toLong())
+            if (pq.size == k) res = maxOf(res, sum * nums[i].second.toLong())
         }
 
         return res
     }
-
-    inner class Num(val num1: Int, val num2: Int)
 }
