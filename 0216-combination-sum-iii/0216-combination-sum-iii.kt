d66b15512169501ff45d68400bbaecab2df985a2
bamboo3042 <39301206+bamboo3042@users.noreply.github.com>
2023-10-05 18:31:48 +0900

    Time: 156 ms (48.42%), Space: 35.3 MB (40.00%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.kt
@@ -0,0 +1,27 @@
+class Solution {
+    
+    val visited = BooleanArray(10) { false }
+    val answer = mutableListOf<List<Int>>()
+    
+    fun combinationSum3(k: Int, n: Int): List<List<Int>> {
+        dfs(k, n, 1, listOf())
+        
+        return answer
+    }
+    
+    fun dfs(k: Int, n: Int, t: Int, l: List<Int>) {
+        if(l.sum() > n) return
+        if(l.size == k) {
+            if(l.sum() == n) answer.add(l)
+            return
+        }
+        
+        for(i in t .. 9) {
+            if(!visited[i]) {
+                visited[i] = true
+                dfs(k, n, i+1, l + i)
+                visited[i] = false
+            }
+        }
+    }
+}
