b2153924895bbcef99787ea624c5aa4717bb4d9f
bamboo3042 <39301206+bamboo3042@users.noreply.github.com>
2024-08-14 17:36:04 +0900

    Time: 178 ms (72.90%), Space: 38.4 MB (80.53%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.kt
+++ 0338-counting-bits/0338-counting-bits.kt
@@ -1,6 +1,6 @@
 class Solution {
     fun countBits(n: Int): IntArray {
-        val ans = IntArray(n+1) {0}
+        val ans = IntArray(n+1)
         
         for (i in 1..n) {
             ans[i] = ans[i shr 1] + (i and 1)
